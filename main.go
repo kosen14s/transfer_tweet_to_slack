f723df0f2e36f88085a2364effaa90bf618f6b42
theoldmoon0602 <theoldmoon0602@gmail.com>
2016-08-15 15:44:34 +0900

    :tada: Initial Commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+    "net/url"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/nlopes/slack"
+)
+
+func  PostMessageToSlack (slackApi *slack.Client, channel slack.Channel, tweetText string) error {
+    /// post message
+    params := slack.NewPostMessageParameters()
+    _, _, err := slackApi.PostMessage(channel.ID, tweetText, params) /// PostMessage function is changed now on github, to rewrite here when package updated
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func main() {
+    if len(os.Args) < 8 {
+        fmt.Fprintln(os.Stderr, "There are 7 arguments required. There is ConsumerKey, ConsumerSecret, UserAccessToken, UserAccessTokenSecret, SlackApiTOken, twitter search word (, spearated), SlackChannelToPostMessage")
+    }
+
+
+    /// TODO: Handling Login Error
+    anaconda.SetConsumerKey(os.Args[1])
+    anaconda.SetConsumerSecret(os.Args[2])
+
+    api := anaconda.NewTwitterApi(os.Args[3], os.Args[4])
+    // api.SetLogger(anaconda.BasicLogger)
+
+
+    /// Slack Api
+    channelName := os.Args[7]
+    slackApi := slack.New(os.Args[5])
+    
+    /// get channel list
+    channels, err := slackApi.GetChannels(false)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "The Slack Api is invalid.")
+        return
+    }
+
+    /// get channel to post message
+    var channel slack.Channel
+    for _, c := range channels {
+        if c.Name == channelName {
+            channel = c
+            break
+        }
+    }
+    if channel.ID == "" {
+        fmt.Fprintln(os.Stderr, "Channel " + channelName + " is not exist.")
+        return
+    }
+
+    /// start streaming
+    v := url.Values{}
+    v.Set("track", os.Args[6])
+    stream := api.PublicStreamFilter(v)
+
+    for {
+        t := <-stream.C
+        switch tweet := t.(type) {
+        case anaconda.Tweet:
+            go PostMessageToSlack(slackApi, channel, "https://twitter.com/statuses/" + tweet.IdStr);
+        }
+    }
+}
